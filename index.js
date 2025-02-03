40410b689f864e1014b0e096048dd57075330890
Alphonse Joseph <42346785+63n713m4n@users.noreply.github.com>
2025-02-03 14:09:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,118 +1,95 @@
-var Typer={
-	text: null,
-	accessCountimer:null,
-	index:0, 
-	speed:2,
-	file:"", 
-	accessCount:0,
-	deniedCount:0, 
-	init: function(){
-		accessCountimer=setInterval(function(){Typer.updLstChr();},500); 
-		$.get(Typer.file,function(data){
-			Typer.text=data;
-			Typer.text = Typer.text.slice(0, Typer.text.length-1);
-		});
-	},
- 
-	content:function(){
-		return $("#console").html();
-	},
- 
-	write:function(str){
-		$("#console").append(str);
-		return false;
-	},
- 
-	addText:function(key){
-		
-		if(key.keyCode == 18){
-			Typer.accessCount++; 
-			
-			if(Typer.accessCount >= 3){
-				Typer.makeAccess(); 
-			}
-		}
-		
-    		else if(key.keyCode == 20){
-			Typer.deniedCount++; 
-			
-			if(Typer.deniedCount >= 3){
-				Typer.makeDenied(); 
-			}
-		}
-		
-    		else if(key.keyCode == 27){ 
-			Typer.hidepop(); 
-		}
-		
-    		else if(Typer.text){ 
-			var cont=Typer.content(); 
-			if(cont.substring(cont.length-1,cont.length) == "|") 
-				$("#console").html($("#console").html().substring(0,cont.length-1)); 
-			if(key.keyCode != 8){ 
-				Typer.index+=Typer.speed;	
-			}
-      		else {
-			if(Typer.index > 0) 
-				Typer.index-=Typer.speed;
-			}
-			var text=Typer.text.substring(0,Typer.index)
-			var rtn= new RegExp("\n", "g"); 
-	
-			$("#console").html(text.replace(rtn,"<br/>"));
-			window.scrollBy(0,50); 
-		}
-		
-		if ( key.preventDefault && key.keyCode != 122 ) { 
-			key.preventDefault()
-		};  
-		
-		if(key.keyCode != 122){ 
-			key.returnValue = false;
-		}
-	},
- 
-	updLstChr:function(){ 
-		var cont=this.content(); 
-		
-		if(cont.substring(cont.length-1,cont.length)=="|") 
-			$("#console").html($("#console").html().substring(0,cont.length-1)); 
-		
-		else
-			this.write("|"); // else write it
-	}
-}
- 
-function replaceUrls(text) {
-	var http = text.indexOf("http://");
-	var space = text.indexOf(".me ", http);
-	
-	if (space != -1) { 
-		var url = text.slice(http, space-1);
-		return text.replace(url, "<a href=\""  + url + "\">" + url + "</a>");
-	} 
-	
-	else {
-		return text
-	}
-}
+var Typer = {
+    text: null,
+    accessCountimer: null,
+    index: 0,
+    speed: 2,
+    file: "63n713m4n.txt",
+    accessCount: 0,
+    deniedCount: 0,
+    
+    init: function(){
+        accessCountimer = setInterval(function(){ Typer.updLstChr(); }, 500); 
+        fetch(Typer.file)
+            .then(response => response.text())
+            .then(data => {
+                Typer.text = data;
+                Typer.text = Typer.text.slice(0, Typer.text.length - 1);
+            });
+    },
 
-Typer.speed=4;
-Typer.file="63n713m4n.txt";
+    content: function(){
+        return document.getElementById("console").innerHTML;
+    },
+
+    write: function(str){
+        document.getElementById("console").innerHTML += str;
+        return false;
+    },
+
+    addText: function(key){
+        if(key.keyCode == 18){
+            Typer.accessCount++; 
+            if(Typer.accessCount >= 3){
+                Typer.makeAccess(); 
+            }
+        }
+        else if(key.keyCode == 20){
+            Typer.deniedCount++; 
+            if(Typer.deniedCount >= 3){
+                Typer.makeDenied(); 
+            }
+        }
+        else if(key.keyCode == 27){ 
+            Typer.hidepop(); 
+        }
+        else if(Typer.text){ 
+            var cont = Typer.content(); 
+            if(cont.substring(cont.length - 1, cont.length) == "|") 
+                document.getElementById("console").innerHTML = cont.substring(0, cont.length - 1); 
+            if(key.keyCode != 8){ 
+                Typer.index += Typer.speed;    
+            } else {
+                if(Typer.index > 0) 
+                    Typer.index -= Typer.speed;
+            }
+            var text = Typer.text.substring(0, Typer.index);
+            var rtn = new RegExp("\n", "g"); 
+            document.getElementById("console").innerHTML = text.replace(rtn, "<br/>");
+            window.scrollBy(0, 50); 
+        }
+        if (key.preventDefault && key.keyCode != 122) { 
+            key.preventDefault();
+        }
+        if(key.keyCode != 122){ 
+            key.returnValue = false;
+        }
+    },
+
+    updLstChr: function(){ 
+        var cont = this.content(); 
+        if(cont.substring(cont.length - 1, cont.length) == "|") 
+            document.getElementById("console").innerHTML = cont.substring(0, cont.length - 1); 
+        else
+            this.write("|");
+    }
+};
+
+Typer.speed = 4;
 Typer.init();
- 
-var timer = setInterval("t();", 30);
-function t() {
-	Typer.addText({"keyCode": 123748});
-	
-	if (Typer.index > Typer.text.length) {
-		clearInterval(timer);
-	}
-}
+
+var timer = setInterval(function() {
+    Typer.addText({ "keyCode": 123748 });
+
+    if (Typer.index > (Typer.text ? Typer.text.length : 0)) {
+        clearInterval(timer);
+    }
+}, 30);
+
 function copyToClipboard(element) {
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val($(element).text()).select();
-  document.execCommand("copy");
-  $temp.remove();
+    var temp = document.createElement("input");
+    document.body.appendChild(temp);
+    temp.value = document.getElementById(element).innerText;
+    temp.select();
+    document.execCommand("copy");
+    temp.remove();
 }
